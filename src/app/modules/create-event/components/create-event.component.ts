3c6b18b310eba2a1c9ebad4885a60a52e3115c31
elifgokdemir <gokdemirelif12@gmail.com>
2024-11-08 15:14:49 +0300

    create event forms added

--- src/app/modules/create-event/components/create-event.component.ts
+++ src/app/modules/create-event/components/create-event.component.ts
@@ -12,5 +12,34 @@ import { NavbarComponent } from '../../layout/components/navbar/navbar.component
   templateUrl: './create-event.component.html',
 })
 export class CreateEventComponent {
-  ngOnInit(): void {}
+  showIndividualEventForm = false;
+  showCorporateEventForm = false;
+  previewUrl: string | ArrayBuffer | null = null;
+
+  openIndividualEventForm() {
+    this.showIndividualEventForm = true;
+    this.showCorporateEventForm = false;
+  }
+
+  openCorporateEventForm() {
+    this.showCorporateEventForm = true;
+    this.showIndividualEventForm = false;
+  }
+
+  closeForms() {
+    this.showIndividualEventForm = false;
+    this.showCorporateEventForm = false;
+    this.previewUrl = null;
+  }
+
+  onFileSelected(event: Event): void {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.previewUrl = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
